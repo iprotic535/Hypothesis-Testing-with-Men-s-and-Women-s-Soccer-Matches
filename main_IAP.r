b8a286e4a95589ad3f611d7e475037b2fc03872c
Ismam Ahmed Protic <iprotic@unr.edu>
2024-10-27 20:41:40 -0700

    Update main_IAP.r

--- main_IAP.r
+++ main_IAP.r
@@ -12,24 +12,30 @@ women = women %>%
 filter(as.Date(date) > "2002-01-01",  tournament == "FIFA World Cup") %>%
 mutate(total_score = home_score+away_score)
 head(women)
-#calculation of bin size for women data using Sturges’ Rule
+#calculation of bin size for women data
 bin_women = max(women$total_score) - min(women$total_score) / (1 + 3.3 * log(nrow(women)))
 bin_women
-#histogram of women and bin was specified manually because it can't be fraction
-ggplot(women, aes(x = total_score), col = "blue") + 
-geom_histogram(bins = 13)
+#histogram of women
+ggplot(women, aes(x = total_score)) + 
+geom_histogram(bins = 13, fill = "lightblue", color = "darkblue") +
+  ggtitle("Total Goals in Women's FIFA World Cup Matches") +
+  xlab("Total Goals") +
+  ylab("Frequency")
 
 #filtering of men data
 men = men %>%
 filter(as.Date(date) > "2002-01-01",  tournament == "FIFA World Cup") %>%
 mutate(total_score = home_score+away_score)
 head(men)
-#calculation of bin size for men data using Sturges’ Rule
+#calculation of bin size for men data
 bin_men = max(men$total_score) - min(men$total_score) / (1 + 3.3 * log(nrow(men)))
 bin_men
-#histogram of men and bin was specified manually because it can't be fraction
-ggplot(men, aes(x = total_score), col = "blue") + 
-geom_histogram(bins = 8)
+#histogram of men
+ggplot(men, aes(x = total_score), color = red) + 
+geom_histogram(bins = 8, fill = "lightcoral", color = "darkred") +
+  ggtitle("Total Goals in Men's FIFA World Cup Matches") +
+  xlab("Total Goals") +
+  ylab("Frequency")
 
 # wilcoxon test for non-parametric two sample test
 a = wilcox.test(men$total_score, women$total_score, alternative = "less")
